dd22bf69885d72a5857f8b3b3f325c39c57cb480
valentins jermakovs <jermakovsvalentins@gmail.com>
2026-08-21 14:10:13 +0300

    Document code

--- app/models/user_role.py
+++ app/models/user_role.py
@@ -1,24 +1,36 @@
 # ==============================
-# Bibliotēku imports
+# Library imports
 # ==============================
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import (
+    Field, 
+    SQLModel
+)
 
 
 # ==============================
-# Lietotāja un lomas saistības modelis
+# User Role Association Model
 # ==============================
 
 class UserRole(SQLModel, table=True):
+    """
+    A model representing the many-to-many relationship between users and roles.
+    
+    Attributes:
+        user_id (int): The ID of the user.
+        
+        role_id (int): The ID of the role.
+    """
+
     __tablename__ = "user_roles"
 
-    # Lietotāja identifikators
+
     user_id: int = Field(
         foreign_key="users.id",
         primary_key=True,
     )
 
-    # Lomas identifikators
+
     role_id: int = Field(
         foreign_key="roles.id",
         primary_key=True,
