f7f0731b4743c9cdb8c8be529db14d9e0ac2aae5
valentins jermakovs <jermakovsvalentins@gmail.com>
2026-08-21 13:24:47 +0300

    Document config file

--- app/config/config.py
+++ app/config/config.py
@@ -1,42 +1,51 @@
 # ==============================
-# Bibliotēku imports
+# Library imports
 # ==============================
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import (
+    BaseSettings, 
+    SettingsConfigDict
+)
 
 
 # ==============================
-# Lietotnes iestatījumi
+# Application settings
 # ==============================
 
 class Settings(BaseSettings):
-
-    # PostgreSQL datubāzes iestatījumi
+    """
+    This class represents the application settings.
+    
+    It uses Pydantic's `BaseSettings` to define the structure of the settings,
+    and `SettingsConfigDict` to configure the .env file.
+    """
+    
+    # PostgreSQL database settings
     POSTGRES_HOST: str
     POSTGRES_PORT: int
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
-
-    # Redis iestatījumi
+    
+    # Redis settings
     REDIS_HOST: str
     REDIS_PORT: int
     REDIS_DB: int
     REDIS_PASSWORD: str | None = None
     REDIS_CACHE_TTL: int = 60
-
-    # JWT tokenu iestatījumi
+    
+    # JWT token settings
     JWT_SECRET_KEY: str
     JWT_ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
-
-    # .env faila konfigurācija
+    
+    # .env file configuration
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
     )
 
 
-# Iestatījumu objekta izveide
+# Create an instance of the settings class
 settings = Settings()
