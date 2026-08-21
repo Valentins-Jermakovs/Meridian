6bb62e250e6dc029c1d52b915f68faae8e620a46
valentins jermakovs <jermakovsvalentins@gmail.com>
2026-08-21 14:16:41 +0300

    Document repositories init file

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -1,8 +1,23 @@
 # ==============================
-# Repozitoriju imports
+# Repository Imports
 # ==============================
 
 from .user import UserRepository
+"""
+A repository for user data storage and retrieval.
+"""
+
 from .role import RoleRepository
+"""
+A repository for role data storage and retrieval.
+"""
+
 from .refresh_token import RefreshTokenRepository
+"""
+A repository for refresh token data storage and retrieval.
+"""
+
 from .audit_log import AuditLogRepository
+"""
+A repository for audit log data storage and retrieval.
+"""
