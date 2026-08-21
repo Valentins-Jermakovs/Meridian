c4843667c2e8216f7121309d3d2196ffb5cda0f4
valentins jermakovs <jermakovsvalentins@gmail.com>
2026-08-21 13:38:35 +0300

    Document facade's init file

--- app/facades/__init__.py
+++ app/facades/__init__.py
@@ -1,7 +1,21 @@
 # ==============================
-# Fasāžu imports
+# Facade imports
 # ==============================
 
 from .auth import AuthFacade
+"""
+This module contains the AuthFacade class, 
+which provides a simple interface for authentication-related functionality.
+"""
+
 from .user import UserFacade
+"""
+This module contains the UserFacade class, 
+which provides a simple interface for user-related functionality.
+"""
+
 from .audit import AuditLogFacade
+"""
+This module contains the AuditLogFacade class, 
+which provides a simple interface for audit log-related functionality.
+"""
